7bcf5339a116b85d861bc7e559228eb1136a1a83
Bilguun <Bilguun040728@gmail.com>
2024-08-19 22:00:43 +0800

    8.19

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import { IoMenu } from "react-icons/io5";
+import { IoInvertModeSharp } from "react-icons/io5";
 
 export const Header = () => {
   return (
-    <div className="block sm:hidden he bg-white justify-between h-0.2 w-0.2 bg-black sm:bg-yellow-400 ">
-      <header className="flex ">
-        <div className="flex items-center justify-between h-20 w-96">
-          <h3 className="font-bold text-slate-800">BRO</h3>
-          <IoMenu />
-        </div>
-      </header>
-    </div>
+    <header className=" sm: flex justify-between content-center ">
+      <h3 className=" font-bold">BRO</h3>
+      <header2 className="sm:text hidden md:flex content-center justify-center gap-8 w-96 h-10">
+        <p className="menu-header">About</p>
+        <p className="menu-header">About</p>
+        <p className="menu-header">About</p>
+        <p className="menu-header">About</p>
+        <IoInvertModeSharp />
+        <IoMenu />
+      </header2>
+    </header>
   );
 };
